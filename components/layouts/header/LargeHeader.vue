97fa55595532cadbae29dbb5b4185f341c325f5e
= <=>
2024-09-04 15:09:58 -0300

    FEAT: Finalização Large Header

--- components/layouts/header/LargeHeader.vue
+++ components/layouts/header/LargeHeader.vue
@@ -1,21 +1,29 @@
 <script setup>
 import AccountCircleOutline from 'vue-material-design-icons/AccountCircleOutline.vue';
 import TicketOutline from 'vue-material-design-icons/TicketOutline.vue';
+import MapMarkerOutline from 'vue-material-design-icons/MapMarkerOutline.vue';
 </script>
 
 <template>
     <div class="cabecalho">
         <header>
-            <img src="../../../public/logoMew.png">
+            <img src="../../../public/logoMew.png" height="60vh">
             <div class="icon">
-                <TicketOutline :size="50"/>
-                <AccountCircleOutline :size="50"/>
+                <TicketOutline :size="45" class="hover"/>
+                <AccountCircleOutline :size="45" class="hover"/>
             </div>
         </header>
         <nav>
-            <p>EM BREVE</p>
-            <p>EM CARTAZ</p>
-     
+            <div class="acessos">
+
+                <p class="hover">EM BREVE </p>
+                <p class="hover">EM CARTAZ</p>
+         
+             
+
+            </div>
+            
+            <p class="hover"><MapMarkerOutline :size="20"/>Mew Theater</p>
         </nav>
        
     </div>
@@ -35,17 +43,36 @@ header{
 }
 nav{
     display: flex;
-    justify-content: space-around;
+    flex-direction: row;
     align-items: center;
+    text-align: center;
+    justify-content: space-between;
     height: 5vh;
     background-color: #490175;
-    color: white;
+    font-family: 'Lexend Mega';
+    font-size: 17px;
+    
 }
 .icon{
     display: flex;
-    justify-content: space-between;
+    gap: 1rem;
     align-items: center;
     width: 5%;
     margin-right: 3vh;
 }
+.acessos{
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+    width: 50%;
+    margin-left: 3vh;
+    margin-right: 3vh;
+   
+    
+}
+.hover:hover{
+    filter: drop-shadow(0 0 5px #DDB1FF);
+    cursor: pointer;
+}
+
 </style>
