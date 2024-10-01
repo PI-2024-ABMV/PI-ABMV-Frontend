2597a9e7b3b46aea86710e796c3a6d93b7bc4ef4
iTzMvz <matheus.vidal.zenere@gmail.com>
2024-10-01 15:12:15 -0300

    FEAT:  Large footer

--- layouts/LayoutLarge.vue
+++ layouts/LayoutLarge.vue
@@ -1,6 +1,8 @@
 <script setup>
 import LargeHeader from '../components/template/header/LargeHeader.vue';
+import LargeFooter from '../components/template/footer/LargeFooter.vue';
 </script>
 <template>
 <LargeHeader/>
+<LargeFooter/>
 </template>
