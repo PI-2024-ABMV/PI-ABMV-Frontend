7bc5964b01e62d42d1815487e891d6e7fa05af10
= <=>
2024-10-01 09:24:09 -0300

    FEAT-3: Adição imagem de apresentação

--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,24 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LayoutFull from '../../layouts/LayoutFull.vue'
 
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: '',
-      component: LayoutFull
+      component: LayoutFull,
+      children: [
+        {
+          path: '/homeee',
+          name: 'home',
+          component: HomeView
+        }
 
+
+
+      ],
     },
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    
   ]
 })
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,5 +3,12 @@
 </script>
 
 <template>
-    <h1>MewMovies</h1>
+<div class="ApreFilme">
+    <img src="../../public/aprefilme.png" alt="">
+</div>
 </template>
+<style scoped>
+.ApreFilme img {
+    height: 57.45vh;  
+}
+</style>
