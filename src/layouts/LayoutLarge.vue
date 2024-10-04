68025e5a27e86ba14d96dddcb818493508bd42a7
iTzMvz <matheus.vidal.zenere@gmail.com>
2024-10-04 12:47:41 -0300

    FIX: Erros de importação

--- src/layouts/LayoutLarge.vue
+++ src/layouts/LayoutLarge.vue
@@ -1,10 +1,9 @@
 <script setup>
 import LargeFooter from '../components/template/large/LargeFooter.vue';
 import LargeHeader from '../components/template/large/LargeHeader.vue';
-import ListagemFilme from '@/components/ListagemFilme.vue';
 </script>
 <template>
 <LargeHeader/>
-<ListagemFilme/>
+<RouterView />
 <LargeFooter/>
 </template>
--- src/layouts/LayoutSmall.vue
+++ src/layouts/LayoutSmall.vue
@@ -1,9 +1,8 @@
 <script setup>
 import SmallHeader from '../components/template/small/SmallHeader.vue';
-import ListagemFilmeMobile from '@/components/ListagemFilmeMobile.vue';
 </script>
 
 <template>
-    <ListagemFilmeMobile/>
+    <RouterView/>
     <SmallHeader />
 </template>
