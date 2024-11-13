58aee4e152d398301674a6aabe53b9de8d2277a2
anaclara06 <anaclara.hahn1@gmail.com>
2024-11-13 14:44:48 -0300

    FEAT: Tela de usuário mobile

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1a3qiil1r6o"
+    "revision": "0.iutsd8fahlo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/template/small/SmallUser.vue
+++ src/components/template/small/SmallUser.vue
@@ -1,6 +1,35 @@
 <script></script>
 <template>
-  <div>
-    <h1>Small User</h1>
-  </div>
+<div class="Container">
+  <h2>Sua Conta</h2>
+  <img src="../../../../public/user.jpeg" alt="">
+  <h1>Miau</h1>
+  <button>Editar</button>
+</div>
 </template>
+<style scoped>
+.Container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 50px;
+  color: #DDB1FF;
+  gap: 1rem;
+}
+img{
+  margin-top: 5vh;
+  width: 200px;
+  border-radius: 50%;
+  margin-bottom: 20px;
+  border: #DDB1FF solid 2px;
+  box-shadow: 0px 0px 10px #DDB1FF;
+}
+button {
+    padding: 10px 15px;
+    background-color: #262424;
+    color: #ddb1ff;
+    border: 1px solid #ddb1ff;
+    border-radius: 10px;
+}
+</style>
