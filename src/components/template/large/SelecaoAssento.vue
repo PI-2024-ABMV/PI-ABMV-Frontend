82cf37dcddbe39ae75d835ea86c087eb6c35d5bd
= <=>
2024-12-10 13:25:33 -0300

    FEAT: Continuação CSS

--- src/components/template/large/SelecaoAssento.vue
+++ src/components/template/large/SelecaoAssento.vue
@@ -33,10 +33,17 @@ const props = defineProps({
             {{ dia }}
           </div>
          
-    
           
           </div>
           <div class="horario">
+            <p>Horário</p>
+            <select name="horarios" id="horarios" placeholder="Selecione um horário">
+              <option value=""> Selecione um horário</option>
+              <option value=""> 13:10</option>
+              <option value=""> 15:00</option>
+              <option value=""> 16:50</option>
+              <option value=""> 18:40</option>
+            </select>
           
           </div>
         </div>
@@ -64,8 +71,9 @@ const props = defineProps({
 .especificacoes {
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
-    margin: 20px;
+    justify-content: left;
+    margin-top: 2vh;
+    height: 12vh;
     width: 70%;
     border: solid 1px #DDB1FF;
     border-radius: 20px;
@@ -75,23 +83,24 @@ const props = defineProps({
     flex-direction: space-between;
     justify-content: center;
     align-items: center;
-    gap: 1vw;
-    padding: 20px;
+    gap: 3vw;
+    padding: 3vh;
     border-right: solid 1px #DDB1FF;
     color: #DDB1FF;
+    width: 60%;
   
 }
 .dia{
     display: flex;
     flex-direction: column;
-    padding: 10px;
+    padding: 1vh;
     border: solid 1px #DDB1FF;
     border-radius: 20px;
     color: #DDB1FF;
-    font-size: 15px;
+    font-size: small;
     gap: 0.5vh;
     width: 3vw;
-    height: 7vh;
+    height: 7.5vh;
     align-items: center;
     box-shadow: 1px 1px 5px #DDB1FF;
 }
@@ -103,5 +112,30 @@ const props = defineProps({
     background-color: #DDB1FF;
     color: white;
     transition: background-color 0.2s, color 0.2s;
+    cursor: pointer;
+}
+.horario{
+  display: flex;
+  flex-direction: column ;
+  width: 40%;
+  padding: 1vw;
+  gap: 1vh;
+  color: #DDB1FF;
+ font-size: small;
+  font-weight: bold;
+}
+.horario select{
+  height: 4vh;
+  background-color: #262424;
+  color: #DDB1FF;
+  border: solid 1px #DDB1FF;
+  border-radius: 15px;
+  font-size: medium;
+  font-weight: bold;
+  padding-left: 1vh;
+  
+}
+select::-webkit-keygen-select{
+  border-style: outset;
 }
 </style>
